f539fa9a0432bbde30aaf62f6203ab9f9fee4409
Andre Toscano <andre.toscano@gmail.com>
2018-08-14 23:02:32 -0300

    adding first version

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+
+
+Vazedit
+
+https://www.ccee.org.br/ccee/documentos/CCEE_433833
+
+
+Hydroedit
+
+https://www.ccee.org.br/ccee/documentos/CCEE_433834
+
+
+
---
+++ test.py
@@ -0,0 +1,28 @@
+
+
+from pyvirtualdisplay import Display as pvDisp
+from Xlib.display import Display
+import pyautogui
+import os
+from easyprocess import EasyProcess
+from time import sleep
+
+display = pvDisp(visible=True, size=(1000, 600))
+# Starting pyvirtualdisplay
+display.start()
+print(os.environ['DISPLAY'])
+
+pyautogui.FAILSAFE = False
+pyautogui._pyautogui_x11._display = Display(os.environ['DISPLAY'])
+print(os.environ['DISPLAY'])
+os.environ['WINEPREFIX'] = '/home/andre/git/vazoes-export-ms/wine-bottle'
+with EasyProcess('/usr/bin/wine /home/andre/git/vazoes-export-ms/thirdparty/VazEdit.exe'):
+    sleep(2)
+    pyautogui.hotkey('tab')
+    pyautogui.hotkey('right')
+    pyautogui.hotkey('space')
+    pyautogui.hotkey('tab')
+    pyautogui.typewrite('oi')
+    sleep(100)
+# Destroying PyVirtualDisplay process
+# display.sendstop()
